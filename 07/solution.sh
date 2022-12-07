91b8eb06af5446c5b9f683127faf2c10a3682ce0
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2022-12-07 00:45:00 -0700

    cleanup

--- 07/solution.sh
+++ 07/solution.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
-# Modify the input by regex
-cp input input.copy
-sed -i '/$ ls/d' input.copy # delete lines containing `ls`
-sed -i '/$ cd \//d' input.copy # stay out of my root directory!
-sed -i '/^[0-9]/s/^/fallocate -l /g' input.copy # allocate file with given size
-sed -i '/dir /d' input.copy # delete output of `ls` that are dir
-sed -i 's/^$ cd \([a-z]*\)/mkdir \1 \&\& cd \1/g' input.copy # create directories before going into them
-sed -i 's/mkdir  && cd ../cd ../g' input.copy # but don't create parent directories
+INPUT="example"
+
+# Modify the $INPUT by regex
+cp $INPUT $INPUT.copy
+sed -i '/$ ls/d' $INPUT.copy # delete lines containing `ls`
+sed -i '/$ cd \//d' $INPUT.copy # stay out of my root directory!
+sed -i '/^[0-9]/s/^/fallocate -l /g' $INPUT.copy # allocate file with given size
+sed -i '/dir /d' $INPUT.copy # delete output of `ls` that are dir
+sed -i 's/^$ cd \([a-z]*\)/mkdir \1 \&\& cd \1/g' $INPUT.copy # create directories before going into them
+sed -i 's/mkdir  && cd ../cd ../g' $INPUT.copy # but don't create parent directories
 
 # Generate the directory tree with files
-chmod +x input.copy
+chmod +x $INPUT.copy
 rm -rf test/
 mkdir -p test/dir_only test/all
 cd test/all
-../../input.copy
+../../$INPUT.copy
 du -b . > ../../results_all_files
 
 # Generate the directory tree without files
 cd ../..
-cp input.copy input.copy.dir_only
-sed -i '/^fallocate/d' input.copy.dir_only
+cp $INPUT.copy $INPUT.copy.dir_only
+sed -i '/^fallocate/d' $INPUT.copy.dir_only
 cd test/dir_only
-../../input.copy.dir_only
+../../$INPUT.copy.dir_only
 du -b . > ../../results_dir_only
 cd ../..
 
